a4cd3703f42a065ae17d024001f9df1d2f06bbac
ehsanbigzad <ehsanbigzad7@gmail.com>
2021-02-11 20:26:09 +0430

    Getx Bottom Navigation implementation

--- lib/pages/dashboard/dashboard_binding.dart
+++ lib/pages/dashboard/dashboard_binding.dart
@@ -1,4 +1,6 @@
 import 'package:get/get.dart';
+import 'package:getx_app/pages/account/account_controller.dart';
+import 'package:getx_app/pages/home/home_controller.dart';
 
 import 'dashboard_controller.dart';
 
@@ -6,5 +8,7 @@ class DashboardBinding extends Bindings {
   @override
   void dependencies() {
     Get.lazyPut<DashboardController>(() => DashboardController());
+    Get.lazyPut<HomeController>(() => HomeController());
+    Get.lazyPut<AccountController>(() => AccountController());
   }
 }
--- lib/themes/light_theme.dart
+++ lib/themes/light_theme.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 ThemeData lightTheme = ThemeData(
   brightness: Brightness.light,
   scaffoldBackgroundColor: Colors.white,
+  highlightColor: Colors.transparent,
+  splashColor: Colors.transparent,
 );
